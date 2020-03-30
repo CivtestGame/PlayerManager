7464038477951447dc7957f4c950a7a76537e0dc
R3R3R3 <24302636+R3R3R3@users.noreply.github.com>
2020-03-30 21:52:02 +0200

    Revision of alt association limits

--- associations.lua
+++ associations.lua
@@ -1,83 +1,120 @@
 
-minetest.register_on_prejoinplayer(function(name, ip)
-      if name:len() >= 20 then
-         -- I think MT protects us against this, but who knows
-         return "Username: '"..name.."' is too long."
+local function toboolean(thing)
+   thing = thing:lower()
+   return ((thing == "t"
+               or thing == "true"
+               or thing == "yes"
+               or thing == true) and true)
+      or false
+end
+
+local function get_associations(player_record)
+   local other_players = pm.find_other_players_with_same_ip(
+      player_record.id, ip
+   )
+
+   if other_players and next(other_players) then
+      local player_shared_ip = player_record.shared_ip
+
+      local alts = {}
+      for _,record in ipairs(other_players) do
+         if not record.shared_ip
+            or not player_shared_ip
+            or record.shared_ip ~= player_shared_ip
+         then
+            alts[#alts + 1] = record.player_name
+         end
       end
 
-      local player_record = pm.get_player_by_name(name)
-      local player_id = player_record and player_record.id
+      if next(alts) then
+         return alts, "This IP address is already tied to account(s): "
+            .. table.concat(alts, ", ") .. ". "
+            .. "Multi-accounting is not allowed on Civtest. "
+            .. "If you think this is an error, please contact "
+            .. "a Civtest administrator on Reddit or Discord."
+      end
 
-      -- Step 1: if a player hasn't yet registered, register them and log their
-      -- IP address.
+      minetest.log(
+         name.." ["..ip.."] bypassed alt check [shared IP with "
+            .. table.concat(alts, ", ") .. "]."
+      )
+   end
+end
+
+minetest.register_on_joinplayer(function(player)
+      -- Handle new players. We have to register new players at this point,
+      -- since this is the only post-authentication join handler. Newbies get
+      -- alt-association checked again, since we now have them in the db.
+      --
+      -- We don't log their IP if they fail the check -- doing so would only
+      -- complicate the job of detangling the alt/IP combos.
+      --
+      local name = player:get_player_name()
+      local player_record = pm.get_player_by_name(name)
       if not player_record then
          player_id = pm.generate_id()
          pm.register_player(name, player_id)
          player_record = pm.get_player_by_name(name)
+
+         local alts, msg = get_associations(player_record)
+         if alts then
+            if combat_tag then
+               combat_tag.set_tag(name, 0)
+            end
+            minetest.kick_player(name, msg)
+            return
+         end
       end
+
       pm.register_ipaddress(ip)
       pm.register_player_ipaddress(player_id, ip)
+end)
 
-      local other_players = pm.find_other_players_with_same_ip(
-         player_id, ip
-      )
 
-      if other_players and next(other_players) then
-         local player_shared_ip = player_record.shared_ip
-
-         local alts = {}
-         for _,record in ipairs(other_players) do
-            if not record.shared_ip
-               or not player_shared_ip
-               or record.shared_ip ~= player_shared_ip
-            then
-               alts[#alts + 1] = record.player_name
-            end
-         end
-
-         if next(alts) then
-            minetest.log(
-               name.." ["..ip.."] tried to log in, but has "
-                  ..tostring(#alts).." associations: "..table.concat(alts, ", ")
-            )
-            return "This IP address is already tied to account(s): "
-               .. table.concat(alts, ", ") .. ". "
-               .. "Multi-accounting is not allowed on Civtest. "
-               .. "If you think this is an error, please contact "
-               .. "a Civtest administrator on Reddit or Discord."
-         end
-
-         minetest.log(
-            name.." ["..ip.."] bypassed alt check [shared IP address: "
-               .. table.concat(alts, ", ") .. "]."
-         )
+minetest.register_on_prejoinplayer(function(name, ip)
+      if name:len() >= 20 then
+         -- I think MT protects us against this, but who knows.
+         return "Username: '"..name.."' is too long."
       end
 
-      local player_dynamic_ip = player_record.dynamic_ip
-      -- db spits out booleans as "t" and "f"
-      player_dynamic_ip = ((player_dynamic_ip == "t") and true) or false
-
-      if player_dynamic_ip then
-         minetest.log(
-            name.." ["..ip.."] bypassed IP check [dynamic IP address]."
-         )
-         pm.register_ipaddress(ip)
-         pm.register_player_ipaddress(player_id, ip)
+      local player_record = pm.get_player_by_name(name)
+      if not player_record then
+         -- New player joined. We handle their IP associations in
+         -- register_on_joinplayer, since this client has not yet authenticated.
          return
       end
 
-      local ip_match = pm.match_player_and_ip(player_id, ip)
+      local player_dynamic_ip = toboolean(player_record.dynamic_ip)
+      if player_dynamic_ip then
+         -- If a player designated as having a dynamic IP ends up on the same
+         -- network as a pre-existing player, well, that's weird. It'll be
+         -- logged anyway.
+         minetest.log(
+            name.." ["..ip.."] bypassed IP check [dynamic IP address]."
+         )
+         return
+      end
 
+      -- Existing accounts with associations are not allowed to connect.
+      local alts, msg = get_associations(player_record)
+      if alts then
+         return msg
+      end
+
+      -- Unless the player is specifed as having a shared or dynamic IP, they
+      -- should only be able connect from their designated, static IP.
+      --
+      -- XXX: I may very well disable this in the future. It doesn't really
+      -- serve much purpose.
+
+      local player_id = player_record.id
+      local ip_match = pm.match_player_and_ip(player_id, ip)
       if not ip_match then
          local ips, shared = pm.get_player_ips(player_id)
          if not ips then
-            pm.register_ipaddress(ip)
-            pm.register_player_ipaddress(player_id, ip)
             minetest.log(
-               name.." ["..ip.."] did not have an IP address assigned. "
-               .. "Using the latest one..."
+               name.." ["..ip.."] did not have an IP address assigned."
             )
-
             return
          end
 
